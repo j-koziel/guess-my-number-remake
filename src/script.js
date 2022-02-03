2b8c0fd13e8f875837df1e7b9b3a769574a42ff4
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-02-03 20:54:41 +0000

    adding initial variables

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
 'use strict';
+// background colour for correct guess - #60b347
 
-console.log('Hello world!');
+// state variables
+let winNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+// selecting DOM elements
+const btnAgain = document.querySelector('.again');
+const btnCheck = document.querySelector('.check');
+
+// Testing if correct elements selected
+btnCheck.addEventListener('click', () => {
+  const guessValue = Number(document.querySelector('.guess').value);
+  console.log(guessValue);
+});
+
+// check if guessed answer is correct
+
+// if not
+
+// init function
