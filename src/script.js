47808e6f1cdd45df28aca13288c16fdc3db038b1
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-02-04 21:23:27 +0000

    Checking if there is no guess

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 'use strict';
 // background colour for correct guess - #60b347
+// 🎉 Correct Number!
+// 📉 Too low!
+// 📈 Too high!
 
 // state variables
 let winNumber = Math.trunc(Math.random() * 20) + 1;
@@ -9,15 +12,32 @@ let highscore = 0;
 // selecting DOM elements
 const btnAgain = document.querySelector('.again');
 const btnCheck = document.querySelector('.check');
+const message = document.querySelector('.message');
+
+// Helper functions
+
+const noNumberMessage = function () {
+  message.textContent = '⛔ No number!';
+};
 
 // Testing if correct elements selected
+// btnCheck.addEventListener('click', () => {
+//   const guessValue = Number(document.querySelector('.guess').value);
+//   console.log(guessValue);
+// });
+
 btnCheck.addEventListener('click', () => {
   const guessValue = Number(document.querySelector('.guess').value);
-  console.log(guessValue);
+  // If there is no value
+  if (!guessValue) noNumberMessage();
+  // check if guessed answer is correct
 });
-
-// check if guessed answer is correct
 
 // if not
 
 // init function
+const init = function () {
+  console.log('Application initialized');
+};
+
+init();
